6ba6e3c5ae6aa84f890d4574dca1816dbe86fd18
null <null>
2023-12-02 07:55:26 +0000

    completed

--- lab6/src/server.c
+++ lab6/src/server.c
@@ -10,7 +10,7 @@
 #include <netinet/ip.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-
+#include "multmodulo.h"
 #include "pthread.h"
 
 struct FactorialArgs {
@@ -19,24 +19,12 @@ struct FactorialArgs {
   uint64_t mod;
 };
 
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-  uint64_t result = 0;
-  a = a % mod;
-  while (b > 0) {
-    if (b % 2 == 1)
-      result = (result + a) % mod;
-    a = (a * 2) % mod;
-    b /= 2;
-  }
-
-  return result % mod;
-}
-
 uint64_t Factorial(const struct FactorialArgs *args) {
   uint64_t ans = 1;
-
-  // TODO: your code here
-
+  for(int i=args->begin;i<args->end;i++ ){
+    ans = (ans * i) % args->mod;
+  }
+  printf("part fact  %ld\n", ans);
   return ans;
 }
 
@@ -67,11 +55,17 @@ int main(int argc, char **argv) {
       switch (option_index) {
       case 0:
         port = atoi(optarg);
-        // TODO: your code here
+        if (port <= 0) {
+          printf("port is a positive number\n");
+          return 1;
+        }
         break;
       case 1:
         tnum = atoi(optarg);
-        // TODO: your code here
+        if (tnum <= 0) {
+          printf("tnum is a positive number\n");
+          return 1;
+        }
         break;
       default:
         printf("Index %d is out of options\n", option_index);
@@ -154,13 +148,13 @@ int main(int argc, char **argv) {
       memcpy(&end, from_client + sizeof(uint64_t), sizeof(uint64_t));
       memcpy(&mod, from_client + 2 * sizeof(uint64_t), sizeof(uint64_t));
 
-      fprintf(stdout, "Receive: %llu %llu %llu\n", begin, end, mod);
+      fprintf(stdout, "Receive: %lu %lu %lu\n", begin, end, mod);
 
       struct FactorialArgs args[tnum];
+      uint64_t delta=end-begin;
       for (uint32_t i = 0; i < tnum; i++) {
-        // TODO: parallel somehow
-        args[i].begin = 1;
-        args[i].end = 1;
+        args[i].begin = begin + i*delta/tnum;
+        args[i].end = begin + (i+1)*delta/tnum;
         args[i].mod = mod;
 
         if (pthread_create(&threads[i], NULL, ThreadFactorial,
@@ -177,7 +171,7 @@ int main(int argc, char **argv) {
         total = MultModulo(total, result, mod);
       }
 
-      printf("Total: %llu\n", total);
+      printf("Total: %lu\n", total);
 
       char buffer[sizeof(total)];
       memcpy(buffer, &total, sizeof(total));
--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -7,56 +7,62 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define SERV_PORT 10050
-#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main() {
-  const size_t kSize = sizeof(struct sockaddr_in);
-
-  int lfd, cfd;
-  int nread;
-  char buf[BUFSIZE];
-  struct sockaddr_in servaddr;
-  struct sockaddr_in cliaddr;
-
-  if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    exit(1);
-  }
-
-  memset(&servaddr, 0, kSize);
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(SERV_PORT);
-
-  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
-    perror("bind");
-    exit(1);
-  }
-
-  if (listen(lfd, 5) < 0) {
-    perror("listen");
-    exit(1);
-  }
-
-  while (1) {
-    unsigned int clilen = kSize;
-
-    if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
-      perror("accept");
-      exit(1);
-    }
-    printf("connection established\n");
-
-    while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-      write(1, &buf, nread);
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s <Port> <BufferSize>\n", argv[0]);
+        exit(1);
     }
 
-    if (nread == -1) {
-      perror("read");
-      exit(1);
+    int SERV_PORT = atoi(argv[1]);
+    int BUFSIZE = atoi(argv[2]);
+    char buf[BUFSIZE];
+    
+    const size_t kSize = sizeof(struct sockaddr_in);
+
+    int lfd, cfd;
+    int nread;
+    struct sockaddr_in servaddr;
+    struct sockaddr_in cliaddr;
+
+    if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
     }
-    close(cfd);
-  }
+
+    memset(&servaddr, 0, kSize);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+
+    if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(lfd, 5) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    while (1) {
+        unsigned int clilen = kSize;
+
+        if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
+            perror("accept");
+            exit(1);
+        }
+        printf("connection established\n");
+
+        while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
+            write(1, &buf, nread);
+        }
+
+        if (nread == -1) {
+            perror("read");
+            exit(1);
+        }
+        close(cfd);
+    }
 }
